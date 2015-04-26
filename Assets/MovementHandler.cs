bc3c57b2332a28e43dfc92b5f306cf15e5fe256f
BladeBreaker <daniel.liverance@gmail.com>
2015-04-25 21:29:31 -0400

    Movement addition
    Made a random script to move character. Works with WASD, Up, Down, Left,
    Right, and joysticks.

---
+++ Assets/MovementHandler.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementHandler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		this.gameObject.transform.Translate(Input.GetAxis("Horizontal") * 1.0f, 0.0f, 0.0f);
+		this.gameObject.transform.Translate(0.0f, Input.GetAxis("Vertical") * 1.0f, 0.0f);
+	}
+}
