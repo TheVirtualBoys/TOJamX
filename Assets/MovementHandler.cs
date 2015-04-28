ddd7ed83384baf7bb351eb7a4013f4fcf510c4f8
SixHourDays <jeffgiffen@gmail.com>
2015-04-27 23:05:37 -0400

    Let's Get Physical
    Level gained 2 edge colliders, one for each platform
    Sprite got duplicated, gained 2d physics and collision
    Original Sprite gained an instant impulse to throw it.
    Phyzaks result.

--- Assets/MovementHandler.cs
+++ Assets/MovementHandler.cs
@@ -6,7 +6,7 @@ public class MovementHandler : MonoBehaviour {
 	// Use this for initialization
 	void Start () 
 	{
-	
+		this.gameObject.GetComponent<Rigidbody2D>().AddRelativeForce( new Vector2( 0.707f, 0.707f ) * 40, ForceMode2D.Impulse );
 	}
 	
 	// Update is called once per frame
