3d75b18741cbef6ea15842e5d11e26291f559cf9
Tim McLennan <tim@nothingness.ca>
2015-05-01 14:58:57 -0400

    Add iTween, Main monobehaviour, Utils, and Timer.

---
+++ Assets/Timer.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer
+{
+	float triggerTime = 0.0f;
+	float elapsedTime = 0.0f;
+
+	// set the callback via
+	event Utils.EmptyCallback callback;
+
+	public Timer(float time, Utils.EmptyCallback func)
+	{
+		triggerTime = time;
+		RegisterCallback(func);
+	}
+
+	public void RegisterCallback(Utils.EmptyCallback func)
+	{
+		callback += func;
+	}
+
+	public void RemoveCallback(Utils.EmptyCallback func)
+	{
+		callback -= func;
+	}
+
+	// Update is called once per frame
+	public void Update(float dt)
+	{
+		elapsedTime += Time.deltaTime;
+		if (elapsedTime >= triggerTime) {
+			callback();
+		}
+	}
+}
---
+++ Assets/Utils.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public static class Utils
+{
+	public delegate void EmptyCallback();
+	public delegate void CallbackEvent<T>(T value);
+	public delegate T ResultEvent<out T>();
+	private static List<Timer> timers = new List<Timer>();
+
+	public static void Update(float dt)
+	{
+		for (int i = 0; i < timers.Count; ++i)
+		{
+			timers[i].Update(dt);
+		}
+	}
+
+	public static Timer AddTimer(float time, EmptyCallback callback)
+	{
+		Timer timer = new Timer(time, callback);
+		timers.Add(timer);
+		return timer;
+	}
+}
