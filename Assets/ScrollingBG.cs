91a5654b5b97a4047e79d18e77e6ce226ea72c9d
Tim McLennan <tim@nothingness.ca>
2015-05-02 13:39:30 -0400

    Add scrolling bg for char select

---
+++ Assets/ScrollingBG.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollingBG : MonoBehaviour
+{
+	public Vector2 speed = new Vector2(1.0f, 1.0f);
+	Vector2 offset       = Vector2.zero;
+	Material mat         = null;
+
+	void Start()
+	{
+		mat = gameObject.GetComponent<MeshRenderer>().materials[0];
+	}
+	
+	void Update()
+	{
+		offset += speed * Time.deltaTime;
+		mat.SetTextureOffset("_MainTex", offset);
+	}
+}
