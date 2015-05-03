2721a40e7713fc340b725c1d755453b57f3b3afb
BladeBreaker <daniel.liverance@gmail.com>
2015-05-02 21:38:52 -0400

    sick music addition
    get that shit in there

---
+++ Assets/PlayMusicOnSceneStart.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayMusicOnSceneStart : MonoBehaviour {
+
+	public AudioClip clipToPlay;
+
+	public float fadeInSeconds = 1.0f;
+	public float fadeOutSeconds = 1.0f;
+
+	// Use this for initialization
+	void Start () 
+	{
+		AudioHandler.PlayMusic(clipToPlay, fadeInSeconds, fadeOutSeconds);
+	}
+}
