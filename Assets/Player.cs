217e7a706a96f1c1cf1fb32d7f1c12e224e0c5e4
SixHourDays <jeffgiffen@gmail.com>
2015-05-02 22:21:07 -0400

    Throw tracking in player

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,19 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 
 
 public class Player : GameplayInputHandler
 {
 	public CharacterFactory.Characters playerClass;
+	public const int arcs = 3;
 	private GameObject m_childPlayerPrefab = null;
-
+	private List<GameObject>[] m_throws = new List<GameObject>[arcs]; //one list per arc
 	public Player m_targetPlayer = null;
+
 
 	// Use this for initialization
 	public override void Start () {
 		base.Start();
 		SetCharacter( playerClass );
+		for( int i = 0; i < arcs; i++ )
+		{
+			m_throws[i] = new List<GameObject>();
+		}
 	}
 	
 	// Update is called once per frame
@@ -23,6 +30,25 @@ public class Player : GameplayInputHandler
 		//HACKJEFFGIFFEN //should be dynamic on stick direction per frame
 		int targetIndex = 1 - (int)playerIndex; //binary invert :-D
 		m_targetPlayer = Main.GetPlayer ( targetIndex );
+
+
+
+		string counts = "- ";
+		for( int i = 0; i < arcs; i++ )
+		{
+			//eliminate those who are ready
+			List<GameObject> throwArc = m_throws[i];
+			while( throwArc.Count > 0 && throwArc[0].GetComponent<ProjectileHandler>().m_seppuku )
+			{
+				Destroy ( throwArc[0] );
+				throwArc.RemoveAt( 0 );
+			}
+
+			counts += m_throws[i].Count;
+			counts += ", ";
+		}
+		Debug.Log ( counts );
+
 	}
 
 	public void SetCharacter( CharacterFactory.Characters which )
@@ -41,13 +67,29 @@ public class Player : GameplayInputHandler
 
 	private void createProjectile( RPSFactory.Type type )
 	{
-		GameObject thing = RPSFactory.GetInst().Create( type );
-		thing.transform.parent = transform;
-		ProjectileHandler ph = thing.GetComponent<ProjectileHandler>();
+		GameObject myGO = RPSFactory.GetInst().Create( type );
+		myGO.transform.parent = transform;
+		ProjectileHandler myPH = myGO.GetComponent<ProjectileHandler>();
+/*		RPSLogic myRPS = myGO.GetComponent<RPSLogic>();
 
-		//set proj arc endpoints
-		ph.start = transform.position;
-		ph.end = m_targetPlayer.transform.position;
+		//figure out if a cancel is possible
+		//search for arc with a possible cancel
+		int arcToCancel = -1;
+		for ( int i = 0; i < arcs; i++ )
+		{
+			List<GameObject> arc = m_targetPlayer.m_throws[i];
+			if ( arc.Count > 0 ) 
+			{
+				GameObject theirGO = arc[arc.Count - 1];
+				RPSLogic theirRPS = theirGO.GetComponent<RPSLogic>();
+
+				if ( myRPS.
+*/
+
+
+		//set proj arc endpoints.  It tells us which arc it went on
+		int whichArc = myPH.SetArc( transform.position, m_targetPlayer.transform.position );
+		m_throws[ whichArc ].Add ( myGO );
 	}
 
 	public override void ThrowRock()
--- Assets/ProjectileHandler.cs
+++ Assets/ProjectileHandler.cs
@@ -4,42 +4,52 @@ using System.Collections;
 public class ProjectileHandler : MonoBehaviour {
 
 	public float duration; //Seconds of travel from start to end
-	public Vector2 start;
+	private Vector2 m_start;
 	public Vector2 middle;
-	public Vector2 end;
-	public int arcs;
+	private Vector2 m_end;
 	public float variance;
 	private float m_startTime;
+	public bool m_seppuku;
 
 	// Use this for initialization
 	void Start () {
 		//this.gameObject.GetComponent<Rigidbody2D>().AddRelativeForce( new Vector2( 0.707f, 0.707f ) * 40, ForceMode2D.Impulse );
 		m_startTime = Time.time;
+		m_seppuku = false;
+	}
 
-		transform.position = start; //avoid flicker from transform start pos to spline start
+	public int SetArc( Vector2 start, Vector2 end )
+	{
+		m_start = start; //avoid flicker from transform start pos to spline start
+		transform.position = start;
+		m_end = end;
 
-		float variation = (float)Random.Range( 0, arcs ) / (float)(arcs - 1); //variation needs to be 0-1 inclusive
+		int whichArc = Random.Range( 0, Player.arcs );
+		float variation = (float)whichArc / (float)(Player.arcs - 1); //variation needs to be 0-1 inclusive
 		
 		//find perpendicular to path
 		Vector2 line = end - start;
 		line.Normalize();
 		Vector2 perpendicular = new Vector2( -line.y, line.x );
-
+		
 		//select which of the arc middles we want
 		Vector2 lowVariant = middle - perpendicular * variance;
 		Vector2 highVariant = middle + perpendicular * variance;
 		middle = Vector2.Lerp( lowVariant, highVariant, variation );
+
+		return whichArc;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		float elapsed = Time.time - m_startTime;
 		float arc = Mathf.Min( elapsed / duration, 1.0f );
-		Vector2 splinePos = Utils.SplineLerp( start, middle, end, arc );
+		Vector2 splinePos = Utils.SplineLerp( m_start, middle, m_end, arc );
 
 		transform.position = new Vector3( splinePos.x, splinePos.y, 1.0f );
 
-		if ( arc == 1.0f ) { Destroy( this.gameObject ); }
+		//if we're done set the flag were ready for honorable death
+		if ( arc == 1.0f ) { m_seppuku = true; }
 
 	}
 }
